22dde354989092900e789855b85665fb88888ba0
Young Hyun <yooyh939@gmail.com>
2017-05-14 17:15:00 +0000

    first

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,57 @@
+class HomeController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def create
+    create_post = Post.new
+    create_post.title = params[:title]
+    create_post.editor = params[:editor]
+    create_post.content = params[:content]
+    create_post.save
+    
+    redirect_to '/'
+  end
+
+  def new
+  end
+
+  def show
+    @view_post = Post.find(params[:post_id])
+  end
+  
+  def update
+    upd_post = Post.find(params[:post_id])
+    upd_post.title = params[:title]
+    upd_post.content = params[:content]
+    upd_post.save
+    redirect_to '/'
+  end
+
+  
+  def update_view
+    @upd_post = Post.find(params[:post_id])
+  end
+  
+  def delete
+    del_post = Post.find(params[:post_id])
+    del_post.destroy
+    redirect_to '/'
+  end
+  
+  def comment_create
+  
+    new_comment=Comment.new(content: params[:content], post_id: params[:post_id])
+    new_comment.save
+    
+    redirect_to '/'
+  end
+  
+  def comment_delete
+    del_comment = Comment.find(params[:comment_id])
+    del_comment.delete
+    
+    redirect_to '/'
+  end
+  
+end
